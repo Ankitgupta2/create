c52862c9c7631d040fb7e7d37e862a11a43aa712
Ankitgupta2 <70169310+Ankitgupta2@users.noreply.github.com>
2021-08-26 10:48:25 +0530

    Add files via upload

---
+++ src/component/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <p>Contact US :</p>
+        <small>@gmail.com</small>{" "}
+      </div>
+      <div className="social">
+        <div>
+          <small>&copy; Copyright 2021</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
